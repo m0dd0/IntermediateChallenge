c460e89962a231e72dc4430d1fb1da83ea24d5cc
Moritz Hesche <mo.hesche@gmail.com>
2021-12-24 14:14:22 +0100

    bugfix

--- run_all.sh
+++ run_all.sh
@@ -6,28 +6,28 @@ do
     sleep 1
     scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Karlsruhe-Durlach&boardType=dep&start=Suche' -a station='Karlruhe Durlach'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Rastatt&boardType=dep&start=Suche' -o station='Rastatt'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Rastatt&boardType=dep&start=Suche' -a station='Rastatt'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Baden-Baden&boardType=dep&start=Suche' -o station='Baden-Baden'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Baden-Baden&boardType=dep&start=Suche' -a station='Baden-Baden'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Freudenstadt&boardType=dep&start=Suche' -o station='Freudenstadt'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Freudenstadt&boardType=dep&start=Suche' -a station='Freudenstadt'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Pforzheim&boardType=dep&start=Suche' -o station='Pforzheim'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Pforzheim&boardType=dep&start=Suche' -a station='Pforzheim'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Mühlacker&boardType=dep&start=Suche' -o station='Muehlacker'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Mühlacker&boardType=dep&start=Suche' -a station='Muehlacker'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Bretten&boardType=dep&start=Suche' -o station='Bretten'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Bretten&boardType=dep&start=Suche' -a station='Bretten'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Bruchsal&boardType=dep&start=Suche' -o station='Bruchsal'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Bruchsal&boardType=dep&start=Suche' -a station='Bruchsal'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Germersheim&boardType=dep&start=Suche' -o station='Germersheim'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Germersheim&boardType=dep&start=Suche' -a station='Germersheim'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Stuttgart&boardType=dep&start=Suche' -o station='Stuttgart'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Stuttgart&boardType=dep&start=Suche' -a station='Stuttgart'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Freiburg&boardType=dep&start=Suche' -o station='Freiburg'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Freiburg&boardType=dep&start=Suche' -a station='Freiburg'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Heidelberg&boardType=dep&start=Suche' -o station='Heidelberg'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Heidelberg&boardType=dep&start=Suche' -a station='Heidelberg'
     sleep 1
-    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Mannheim&boardType=dep&start=Suche' -o station='Mannheim'
+    scrapy crawl db -o db_data_1.csv -a url='https://reiseauskunft.bahn.de/bin/bhftafel.exe/dn?country=DEU&protocol=https:&rt=1&input=Mannheim&boardType=dep&start=Suche' -a station='Mannheim'
     sleep 600
 done
