ed8fef381a99ffc7952100f354cae336be0fd528
Moritz Hesche <mo.hesche@gmail.com>
2021-12-24 14:53:22 +0100

    updated scriot

--- run_mulit.sh
+++ run_mulit.sh
@@ -1,1 +1,8 @@
-scrapy crawl db_multi_station -o ./data/test.csv -a stations="$(<./dbScrapper/stations.json)"
+
+#!/bin/bash
+
+while true
+do
+  scrapy crawl db_multi_station -o ./data/db_data_2.csv -a stations="$(<./dbScrapper/stations.json)"
+  sleep 600
+done
